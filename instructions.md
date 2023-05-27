6278d29cdf8b9dc8293d1bba526399d3240b9f9e
Nathan <nathan.watson.172@gmail.com>
2023-05-27 10:25:19 +0100

    Made it work (see 'instructions.md')

---
+++ instructions.md
@@ -0,0 +1,7 @@
+# How to Use
+
+-   run `npm install`
+-   run `"./node_modules/.bin/webpack"`
+-   run `serve dist/`
+-   go to the address given
+-   check the console in inspect to see the output!
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+//import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+//import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 console.log('working!!!')
 
