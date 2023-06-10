3bba5426bf3a2c3633bf5552da1a0be014126b87
Nathan <nathan.watson.172@gmail.com>
2023-06-10 11:43:40 +0100

    Attempted microsoft oath NON-WORKING

--- dist/test/microsoft-auth/src/index.js
+++ dist/test/microsoft-auth/src/index.js
@@ -1,5 +1,34 @@
 import { OAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyDhotO15Sh5GMBR1Eo_Ol83W70ndyYcCvA",
+    authDomain: "bcp-shop-3cf1e.firebaseapp.com",
+    projectId: "bcp-shop-3cf1e",
+    storageBucket: "bcp-shop-3cf1e.appspot.com",
+    messagingSenderId: "180818875291",
+    appId: "1:180818875291:web:3a11cb9065560aed0a873e",
+    measurementId: "G-QYXLV35DG1"
+});
+
 
 const provider = new OAuthProvider('microsoft.com');
+provider.setCustomParameters({
+    prompt: "consent",
+});
+
 const auth = getAuth();
-signInWithPopup
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // User is signed in.
+    // IdP data available in result.additionalUserInfo.profile.
+
+    // Get the OAuth access token and ID Token
+    const credential = OAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+    const idToken = credential.idToken;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
