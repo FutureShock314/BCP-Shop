093bc268e7090a31ead2bbe49482d5cda1a1f7c9
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-06-12 12:10:13 +0000

    Tried to add sign-out button to home page

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \*********************/
 /***/ (() => {
 
-eval("document.getElementsByClassName('sign-in')[0].addEventListener(\"click\", () => {\r\n    window.location = './login'\r\n});\n\n//# sourceURL=webpack://bcp-shop/./src/main.js?");
+eval("document.getElementsByClassName('sign-in')[0].addEventListener(\"click\", () => {\n    window.location = './login'\n});\n\n//# sourceURL=webpack://bcp-shop/./src/main.js?");
 
 /***/ })
 
