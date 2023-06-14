6ac0f4866ef611cee84be19f4aca741892b096ce
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-06-14 11:51:18 +0000

    Removed redundant code

--- src/purchase-test.js
+++ src/purchase-test.js
@@ -1,3 +1,5 @@
+console.log("working (purchase-test)");
+
 var items = [
     'football',
     'pen'
@@ -23,36 +25,3 @@ for(var i = 0; i < count; i++) {
     console.log(i + item);
 }
 console.log('finished');
-
-
-
-
-
-console.log("working (purchase-test)");
-
-const o_modal = document.querySelector('.football-modal');
-const o_openModal = document.querySelector('.football-modal-open');
-const o_closeModal = document.querySelector('.football-modal-close');
-
-o_openModal.addEventListener('click', () => {
-    o_modal.showModal();
-});
-
-o_closeModal.addEventListener('click', () => {
-    o_modal.close();
-});
-
-// * play anim for hiding modal
-
-closeModal.addEventListener('click', () => {
-    modal.classList.add('hide');
-    console.log('added hide');
-    modal.addEventListener('animationend', function(){
-        console.log('added event listener');
-        modal.classList.remove('hide');
-        console.log('removed "hide"');
-        modal.close();
-        modal.removeEventListener('animationend',  arguments.callee, false);
-        console.log('removed animationend check');
-    }, false);
-});
